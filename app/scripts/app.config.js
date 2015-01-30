e64114056d1d01cb28340d72a6e36b4962d93961
Chase Simpson <chase.w.simpson1@gmail.com>
2015-01-29 23:21:46 -0500

    added a crap ton of stuff

---
+++ app/scripts/app.config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('angAuthApp').config(function($urlRouterProvider, $stateProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+
+    .state('main', {
+      url: '/',
+      templateUrl: '/views/main.html'
+    })
+
+    .state('register', {
+      url: '/register',
+      templateUrl: '/views/register.html',
+      controller: 'RegisterCtrl'
+    });
+
+});
---
+++ app/scripts/app.js
@@ -0,0 +1,4 @@
+'use strict';
+
+angular
+  .module('angAuthApp', ['ui.router']);
