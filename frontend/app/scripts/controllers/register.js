6dbe74c66c9532542b1348d5a8a59c9bfa41e0f8
Chase Simpson <chase.w.simpson1@gmail.com>
2015-01-30 17:26:39 -0500

    node express, mongoDB added for server side API register posts

--- frontend/app/scripts/controllers/register.js
+++ frontend/app/scripts/controllers/register.js
@@ -4,8 +4,12 @@ angular.module('angAuthApp')
   .controller('RegisterCtrl', function ($scope, $rootScope, $http, alert) {
     $scope.submit = function () {
 
-      var url = '/';
-      var user = {};
+      var url = 'http://localhost:3000/register';
+      var user = {
+        email: $scope.email,
+        password: $scope.password
+      };
+
       $http.post(url, user)
         .success(function(res) {
           alert('success', 'OK!', ' You are now registered')
